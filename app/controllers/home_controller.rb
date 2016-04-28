c7c0a835b8284ef5f4317dbbebb2324ac2b4d858
tkumor3 <tkumor3@gmail.com>
2016-04-28 21:03:50 +0200

    background

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+    before_action :check_auth
     def index
 
+    end
+
+    def check_auth
+        unless current_user.authorization.nil?
+            redirect_to current_user
+        end
     end
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -4,6 +4,6 @@ Instagram.configure do |config|
     config.client_id = "a5fdb1d646764e5cbc82d0a250b99abb"
     config.client_secret = "6a261b89d24b4fcf9d34ff265ac7bc7e"
 end
-CALLBACK_URL = "https://inb.herokuapp.com/callback"
+#CALLBACK_URL = "https://inb.herokuapp.com/callback"
 
-#CALLBACK_URL = "http://localhost:3000/callback"
+CALLBACK_URL = "http://localhost:3000/callback"
