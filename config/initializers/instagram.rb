76308c19678e2cd8c0fdcb546c9c8581da5efbe9
tkumor3 <tkumor3@gmail.com>
2016-04-28 12:51:06 +0200

    heroku url

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -3,6 +3,7 @@ require "instagram"
 Instagram.configure do |config|
     config.client_id = "a5fdb1d646764e5cbc82d0a250b99abb"
     config.client_secret = "6a261b89d24b4fcf9d34ff265ac7bc7e"
-    CALLBACK_URL = "https://inb.herokuapp.com/callback"
 end
+CALLBACK_URL = "https://inb.herokuapp.com/callback"
 
+#CALLBACK_URL = "http://localhost:3000/callback"
