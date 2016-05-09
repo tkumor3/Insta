bb726d452ab34609d18172dab5032fb6cf0b49ed
tkumor3 <tkumor3@gmail.com>
2016-05-09 13:38:18 +0200

    clean

--- test/mailers/previews/user_m_preview.rb
+++
@@ -1,7 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_m
-class UserMPreview < ActionMailer::Preview
-    def sample_mail_preview
-        ExampleMailer.sample_email(User.first)
-    end
-
-end
--- test/models/followed_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FollowedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
