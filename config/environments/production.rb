ec175051e0c17a4adb4bd06aa871181f2191e86a
tkumor3 <tkumor3@gmail.com>
2016-05-17 16:01:58 +0200

    heroku conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.precompile += %w( *.js *.css )
+  config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
